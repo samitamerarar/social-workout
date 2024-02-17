36c48b44e15c63cc81728ede107766f7c8b76bd8
samitamerarar <sami.t.arar@hotmail.com>
2024-02-17 17:01:35 -0500

    Added Logger config to output into a log file

--- socialworkoutapi/logging_conf.py
+++ socialworkoutapi/logging_conf.py
@@ -13,6 +13,11 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                     "format": "%(name)s:%(lineno)d - %(message)s"
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)04dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s"
                 }
             },
             "handlers": {
@@ -20,14 +25,23 @@ def configure_logging() -> None:
                     "class": "rich.logging.RichHandler",  # pip install rich (for datetime en colors output)
                     "level": "DEBUG",
                     "formatter": "console"
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "socialworkoutapi.log",
+                    "maxBytes": 1024 * 1024,  # 1MB
+                    "backupCount": 2,  # how many files
+                    "encoding": "utf8"
                 }
             },
             "loggers": {
-                "uvicorn": {"handlers": ["default"], "level": "INFO"},
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
                 "databases": {"handlers": ["default"], "level": "WARNING"},
                 "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
                 "socialworkoutapi": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     # propagate = False : dont send to the root logger (e.g. root.socialworkoutapi.routers.post)
                     "propagate": False
